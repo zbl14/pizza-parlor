5a3e75192fc2c704dc9b622f80c345c29c07f100
zbl14 <ifthereisoneday@gmail.com>
2022-06-17 13:44:47 -0700

    display subtotal, tax, tips and total

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ function Pizza(name, addr, toppings, size){
   this.size = size;
 }
 
+let myPizza = new Pizza();
 
 Pizza.prototype.cost = function() {
   let cost;
@@ -18,14 +19,9 @@ Pizza.prototype.cost = function() {
   return cost
 }
 
-
-// let myPizza = new Pizza("name", "deliveryAddr", [], "orederedsize");
-
-
 $(document).ready(function() {
   $("form#orderInfo").submit(function(event) {
     event.preventDefault();
-    
     const inputtedName = $("input#inputtedName").val(); 
     const inputtedAddr = $("input#inputtedAddr").val(); 
     const selectedSize = $("input:radio[name=sizeSurvey]:checked").val();
@@ -33,12 +29,30 @@ $(document).ready(function() {
     $("input:checkbox[name=toppingsList]:checked").each(function() {
       selectedToppings.push($(this).val());
     });
+    const tipsPct = parseFloat($("input:radio[name=tipsPct]:checked").val());
     let myPizza = new Pizza(inputtedName, inputtedAddr, selectedToppings, selectedSize);
-    let total = myPizza.cost()
+    let cost = parseFloat(myPizza.cost().toFixed(2));
+    let tax = parseFloat((cost * 0.075).toFixed(2));
+    let tips = parseFloat(((cost + tax) * tipsPct).toFixed(2));
+    let total = cost + tax + tips
+  
+    
+  
+    
+  
+    // $("#tips").click(function(){
+
+    // });
+
+
+
     $(".name").html(myPizza.name);
     $(".addr").html(myPizza.addr);
     $(".size").html(myPizza.size);
     $(".toppings").html(myPizza.toppings.join(", "));
+    $(".cost").html("$ " + cost);
+    $(".tax").html("$ " + tax);
+    $(".tips").html("$ " + tips);
     $(".total").html("$ " + total);
   });
 });
